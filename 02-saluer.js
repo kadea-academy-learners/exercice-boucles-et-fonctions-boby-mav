b08f2a0fdd477316ed7280751d675ead0dcfbddb
MAVUNZA BOBETTE <bobettemavunza4@gmail.com>
2025-10-23 05:27:53 +0100

    Ajout de la fonction saluer

--- 02-saluer.js
+++ 02-saluer.js
@@ -1,18 +1,9 @@
-/*
-  Énoncé :
-  Écris une fonction nommée `saluer` qui prend un paramètre `nom`.
-  - Si `nom` est fourni, afficher : "Bonjour, [nom]"
-  - Si aucun nom n'est fourni, afficher : "Bonjour, inconnu"
-
-  Signature attendue :
-    function saluer(nom) -> void
-
-  Placeholder : levée d'erreur pour indiquer que l'exercice doit être implémenté.
-*/
-function saluer(nom) {
-  // Exercice non implémenté : afficher Bonjour, [nom] ou Bonjour, inconnu
-  // Placeholder neutre : ne fait rien et retourne undefined.
+function saluer (nom) {
+  if (!nom) {
+    console.log("Bonjour, inconnu");
+  }
+  else {
+    console.log("Bonjour, " + nom);
+  }
 }
-
-// Ne pas modifier la ligne ci-dessous
-module.exports = { saluer }
+module.exports = { saluer };
